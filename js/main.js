185115a3183c55e9dd8d2a628d02068b9b55417a
Patrick Hund <pahund@ebay.com>
2014-11-04 10:07:46 +0100

    added files from full screen demo, based on HTML5 boilerplate
    Change-Id: I443736e61d57da84f7ecca621ebc030c2e34a8e2

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    var fancyOpen = false;
+
+    $(document).on("webkitfullscreenchange mozfullscreenchange iefullscreenchange fullscreenchange", function () {
+        console.log("[PH_LOG] fullscreen change"); // PH_TODO: REMOVE
+        if (fancyOpen) {
+            $.fancybox.close();
+        } else {
+            $.fancybox.open([
+                {
+                    href: "img/big_image_1.jpg",
+                    title: "schöne Landschaft"
+                },
+                {
+                    href: "img/big_image_2.jpg",
+                    title: "noch schönere Landschaft"
+                },
+                {
+                    href: "img/big_image_3.jpg",
+                    title: "extrem schöne Landschaft"
+                }
+            ], {
+                afterShow: function () {
+                    $(".fancybox-close").click(function () {
+                        console.log("[PH_LOG] close"); // PH_TODO: REMOVE
+                        var request = document.exitFullscreen
+                            || document.webkitExitFullscreen
+                            || document.mozCancelFullScreen
+                            || document.msExitFullscreen;
+
+                        if (typeof request !== 'undefined' && request) {
+                            console.log("[PH_LOG] er ist defined"); // PH_TODO: REMOVE
+                            request.call(document);
+                        }
+                    });
+                }
+            });
+        }
+        fancyOpen = !fancyOpen;
+    });
+
+
+    $("#fancyBoxOpener").click(function () {
+        var docElement = document.documentElement,
+            request;
+
+        request = docElement.requestFullScreen
+            || docElement.webkitRequestFullScreen
+            || docElement.mozRequestFullScreen
+            || docElement.msRequestFullScreen;
+
+        if (typeof request !== "undefined" && request) {
+            request.call(docElement);
+        }
+
+
+    });
+
+});
